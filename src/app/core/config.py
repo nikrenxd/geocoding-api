8ed070353dade0e333f4b8b15116b527194a1615
nikrenxd <nikrenc1101@gmail.com>
2025-08-24 23:30:18 +0300

    add TEST_DB_URL and ENV mode in config/Settings

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,8 +1,12 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    ENV: str = Literal["DEV", "PROD", "TEST"]
     DB_URL: str
+    TEST_DB_URL: str
     API_KEY: str
     model_config = SettingsConfigDict(env_file=".env")
 
