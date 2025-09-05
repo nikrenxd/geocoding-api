ef653ac40d81b21fb1f08c261aef759ba72d462c
nikrenxd <nikrenc1101@gmail.com>
2025-09-05 12:14:23 +0300

    replace GeodataModel dataclass with _convert_json_data method

--- src/app/services.py
+++ src/app/services.py
@@ -1,21 +1,8 @@
-from dataclasses import asdict, dataclass, fields
-from typing import Self
-
 from httpx import AsyncClient
 
 from src.app.core.config import Settings
+from src.app.models import Geodata
 from src.app.repositories import GeodataRepository
-
-
-@dataclass(frozen=True)
-class GeodataModel:
-    display_name: str
-    lat: float
-    lon: float
-
-    @classmethod
-    def from_dict(cls, data: dict) -> Self:
-        return cls(*[data.get(field.name) for field in fields(cls)])
 
 
 class GeodataService:
@@ -24,12 +11,21 @@ class GeodataService:
         self.config = config
 
     @staticmethod
-    def _validate_data_list(data: list[dict]) -> list[dict]:
-        return [asdict(GeodataModel.from_dict(d)) for d in data]
+    def _convert_json_data(data: dict) -> dict:
+        dict_keys = ("display_name", "lat", "lon")
+        dict_copy = data.copy()
 
-    @staticmethod
-    def _validate_data(data: dict) -> dict:
-        return asdict(GeodataModel.from_dict(data))
+        new_data = {key: dict_copy[key] for key in dict_keys}
+        new_data["lat"] = float(new_data["lat"])
+        new_data["lon"] = float(new_data["lon"])
+
+        return new_data
+
+    def _validate_data_list(self, data: list[dict]) -> list[dict]:
+        return [self._convert_json_data(d) for d in data]
+
+    def _validate_data(self, data: dict) -> dict:
+        return self._convert_json_data(data)
 
     async def _request_data_from_api(
         self, many: bool, **url_params
@@ -49,20 +45,18 @@ class GeodataService:
             return self._validate_data_list(response_data)
         return self._validate_data(response_data)
 
-    async def get_all_data(self) -> list[GeodataModel]:
+    async def get_all_data(self) -> list[Geodata]:
         return await self.repository.get_all()
 
     async def get_location_data_from_location_name(
         self, **url_params
-    ) -> list[GeodataModel] | GeodataModel | None:
+    ) -> list[Geodata] | Geodata | None:
         response = await self._request_data_from_api(True, **url_params)
         if len(response) == 0:
             return None
         return await self.repository.save_data_list(response)
 
-    async def get_location_data_from_coords(
-        self, **url_params
-    ) -> GeodataModel | None:
+    async def get_location_data_from_coords(self, **url_params) -> Geodata | None:
         response = await self._request_data_from_api(False, **url_params)
         if not response:
             return None
