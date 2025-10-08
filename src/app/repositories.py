9628f774194a62f8942ce84ba4f2f98b96a1469e
nikrenxd <nikrenc1101@gmail.com>
2025-10-08 18:00:28 +0300

    remove get_all, save_data_list from repository

--- src/app/repositories.py
+++ src/app/repositories.py
@@ -1,4 +1,4 @@
-from sqlalchemy import insert, select
+from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.app.models import Geodata
@@ -7,19 +7,6 @@ from src.app.models import Geodata
 class GeodataRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
-
-    async def get_all(self) -> list[Geodata]:
-        stmt = select(Geodata).distinct()
-        result = await self.session.execute(stmt)
-        return result.scalars().all()
-
-    async def save_data_list(self, data: list[dict]) -> list[Geodata] | None:
-        result = await self.session.execute(
-            insert(Geodata).returning(Geodata),
-            [*data],
-        )
-        await self.session.commit()
-        return result.scalars().all()
 
     async def save_data(self, data: dict) -> Geodata | None:
         stmt = insert(Geodata).values(**data).returning(Geodata)
