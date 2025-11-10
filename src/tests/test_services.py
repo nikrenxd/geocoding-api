8e3e6704ce5a0a7ed952b866987d25dedb7955b1
nikrenxd <nikrenc1101@gmail.com>
2025-11-10 10:59:21 +0200

    update test_services

--- src/tests/test_services.py
+++ src/tests/test_services.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.app.services import GeodataService
+from src.app.services.geodata.service import GeodataService
 
 
 @pytest.mark.anyio
@@ -23,9 +23,9 @@ from src.app.services import GeodataService
     ],
 )
 async def test_get_location_data_from_coords(
-    geodata_repository, config, exp_location_name, lat, lon
+    geodata_repository, geodata_client, config, exp_location_name, lat, lon
 ):
-    geodata_service = GeodataService(geodata_repository, config)
+    geodata_service = GeodataService(geodata_repository, config, geodata_client)
     res = await geodata_service.get_location_data_from_coords(lat=lat, lon=lon)
 
     assert res.display_name == exp_location_name
@@ -39,26 +39,25 @@ async def test_get_location_data_from_coords(
             "New York City Hall, 260, Broadway,"
             + " Lower Manhattan, Manhattan Community Board 1,"
             + " Manhattan, New York County, New York, 10000, United States",
-            40.7127,
-            -74.0059,
+            40.71273945,
+            -74.00593904130275,
         ),
         (
             "Fleming Way, London Borough of Bexley,"
             + " London, Greater London, England, SE28 8NS, United Kingdom",
-            51.5074,
-            0.1283,
+            51.5073599,
+            0.1283349,
         ),
     ],
 )
 async def test_get_location_data_from_location_name(
-    geodata_repository, config, location_name, exp_lat, exp_lon
+    geodata_repository, geodata_client, config, location_name, exp_lat, exp_lon
 ):
-    geodata_service = GeodataService(geodata_repository, config)
+    geodata_service = GeodataService(geodata_repository, config, geodata_client)
     res = await geodata_service.get_location_data_from_location_name(
-        q=location_name
+        query=location_name,
     )
 
-    assert len(res) != 0
-    assert res[0].display_name == location_name
-    assert round(res[0].lat, 4) == exp_lat
-    assert round(res[0].lon, 4) == exp_lon
+    assert res.display_name == location_name
+    assert res.lat == exp_lat
+    assert res.lon == exp_lon
